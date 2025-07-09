145d8fbcd686fc511f7a8df1aa1e92d600c347ef
OlubodeMichael <moolubode3@gmail.com>
2025-07-09 12:14:58 -0400

    Update frontend for push to main

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,5 +1,12 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  theme: {
+    extend: {
+      fontFamily: {
+        amsterdamer: ["Amsterdamer Garamond", "serif"],
+      },
+    },
+  },
 };
 
 export default config;
